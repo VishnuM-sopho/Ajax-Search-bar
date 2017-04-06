54ded43d51b7100170976c9ba847a3d8a4baae1b
Vishnu Mv <vishnum1998@gmail.com>
2017-04-06 18:39:18 +0000

    a

--- public_html/display.php
+++
@@ -1,7 +0,0 @@
-<?php require("../views/title.php"); ?>
-
-<?php
-require("../controllers/config.php"); 
-    $_SESSION["id"]=$_GET['id'];
-    render("/portfolio.php",["id" => $_SESSION["id"],"change" => $_GET["change"]]);
-?>
